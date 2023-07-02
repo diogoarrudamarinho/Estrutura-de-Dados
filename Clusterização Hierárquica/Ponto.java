7a7d12ccb610218cd2879938f78402a5633d84aa
Diogo Arruda <88099979+diogoarrudamarinho@users.noreply.github.com>
2023-07-02 15:47:27 -0300

    Update Ponto.java

--- Clusterização Hierárquica/Ponto.java
+++ Clusterização Hierárquica/Ponto.java
@@ -8,18 +8,6 @@ public class Ponto {
         this.y = y;
     }
 
-    public double calculaDistancia(Ponto p){
-        return Math.sqrt( Math.abs( (x - p.x) + (y - p.y) ) );
-    }
-
-    public Ponto retornaMedia(Ponto p){
-
-        double x = (this.x + p.x) / 2;
-        double y = (this.y + p.y) / 2;
-
-        return new Ponto(x, y);
-    }
-
     public double getX(){
         return x;
     }
